b95c795ea63a67aec6f41b6f374e56d7d1b9baeb
joey <j.tan2231@gmail.com>
2024-02-12 17:12:07 -0600

    node focusing

--- src/components/graph.jsx
+++ src/components/graph.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "../styles/graph.css";
 
 const NODE_DIAMETER = 50;
+const UNFOCUSED_OPACITY = 0.1;
 
 function getRandom(max) {
     return Math.random() * max;
@@ -18,9 +19,12 @@ function Node(props) {
         setPosition(props.position);
     }, [props.position]);
 
+    const stopPropagation = (event) => {
+        event.stopPropagation();
+    };
+
     return (
         <div
-            key={props.key}
             className="node"
             style={{
                 position: "absolute",
@@ -29,7 +33,14 @@ function Node(props) {
                 width: `${NODE_DIAMETER}px`,
                 height: `${NODE_DIAMETER}px`,
                 zIndex: "2",
+                cursor: "pointer",
+                opacity: `${props.opacity}`,
             }}
+            onMouseDown={stopPropagation}
+            onMouseUp={stopPropagation}
+            onMouseMove={stopPropagation}
+            onMouseEnter={() => props.focusCallback.enter(props.nodeKey)}
+            onMouseLeave={props.focusCallback.exit}
         />
     );
 }
@@ -38,9 +49,12 @@ export function Graph(props) {
     const [nodePositions, setNodePositions] = useState([]);
     // this is probably a temporary variable
     const [edges, setEdges] = useState([]);
+    const [edgeMap, setEdgeMap] = useState({});
     const [mouseDown, setMouseDown] = useState(null);
 
     const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
+
+    const [focusKey, setFocusKey] = useState(-1);
 
     // grid sizes
     const gridSquareSize = { x: 100, y: 100 };
@@ -63,7 +77,7 @@ export function Graph(props) {
         occupancyGrid[i] = new Array(gridSize.y).fill(false);
     }
 
-    // { x: number (pixels), y: number (pixels) }
+    // { ...position, x: number (pixels), y: number (pixels) }
     const isOccupied = (position) => {
         const [x, y] = pixelsToGrid(position);
         return occupancyGrid[x][y];
@@ -79,9 +93,17 @@ export function Graph(props) {
 
         let newNodes = [];
         for (let i = 0; i < 10; i++) {
-            let candidate = { x: getRandom(gridSizePixels.x), y: getRandom(gridSizePixels.y) };
+            let candidate = {
+                key: i,
+                x: getRandom(gridSizePixels.x),
+                y: getRandom(gridSizePixels.y),
+            };
             while (isOccupied(candidate)) {
-                candidate = { x: getRandom(gridSizePixels.x), y: getRandom(gridSizePixels.y) };
+                candidate = {
+                    ...candidate,
+                    x: getRandom(gridSizePixels.x),
+                    y: getRandom(gridSizePixels.y),
+                };
             }
 
             const [x, y] = pixelsToGrid(candidate);
@@ -96,17 +118,23 @@ export function Graph(props) {
     // create edge elements from list of positions
     const generateEdges = (positions) => {
         let edges = [];
+        let edgeMap = {};
         for (let i = 0; i < positions.length; i++) {
+            const p1 = positions[i];
+            edgeMap[p1.key] = [];
             for (let j = 0; j < positions.length; j++) {
-                if (i != j && coinFlip()) {
-                    const [p1, p2] = [positions[i], positions[j]];
+                if (i !== j && coinFlip()) {
+                    const p2 = positions[j];
                     const length = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
                     const angle = Math.atan2(p2.y - p1.y, p2.x - p1.x) * (180 / Math.PI);
 
-                    edges.push({ length, angle, x: p1.x, y: p1.y });
+                    edges.push({ from: p1.key, to: p2.key, length, angle, x: p1.x, y: p1.y });
+                    edgeMap[p1.key].push(p2.key);
                 }
             }
         }
+
+        setEdgeMap(edgeMap);
 
         return edges;
     };
@@ -131,6 +159,7 @@ export function Graph(props) {
                 onMouseUp={() => {
                     setNodePositions(
                         nodePositions.map((p) => ({
+                            ...p,
                             x: p.x + dragOffset.x,
                             y: p.y + dragOffset.y,
                         })),
@@ -152,24 +181,44 @@ export function Graph(props) {
                 {nodePositions.map((position, index) => (
                     <Node
                         key={index}
+                        nodeKey={index}
                         position={{ x: position.x + dragOffset.x, y: position.y + dragOffset.y }}
-                    />
-                ))}
-                {edges.map((e) => (
-                    <div
-                        style={{
-                            zIndex: "1",
-                            transformOrigin: "top left",
-                            transform: `translate(${e.x + dragOffset.x + NODE_DIAMETER / 2}px, ${e.y + dragOffset.y + NODE_DIAMETER / 2}px) rotate(${e.angle}deg)`,
-                            width: `${e.length}px`,
-                            height: "2px",
-                            left: "0",
-                            top: "0",
-                            position: "absolute",
-                            backgroundColor: "#ddd",
+                        focusCallback={{
+                            enter: (key) => {
+                                setFocusKey(key);
+                            },
+                            exit: () => setFocusKey(-1),
                         }}
+                        opacity={
+                            focusKey === -1 ||
+                            index === focusKey ||
+                            edgeMap[focusKey].includes(index)
+                                ? 1
+                                : UNFOCUSED_OPACITY
+                        }
                     />
                 ))}
+                {edges.map((e) => {
+                    return (
+                        <div
+                            style={{
+                                zIndex: "1",
+                                transformOrigin: "top left",
+                                transform: `translate(${e.x + dragOffset.x + NODE_DIAMETER / 2}px, ${e.y + dragOffset.y + NODE_DIAMETER / 2}px) rotate(${e.angle}deg)`,
+                                width: `${e.length}px`,
+                                height: "2px",
+                                left: "0",
+                                top: "0",
+                                opacity:
+                                    e.from === focusKey || focusKey === -1
+                                        ? "1"
+                                        : `${UNFOCUSED_OPACITY}`,
+                                position: "absolute",
+                                backgroundColor: "#888",
+                            }}
+                        />
+                    );
+                })}
             </div>
             <button onClick={regenerate}>regen</button>
         </>
